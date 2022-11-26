578aa52d7675315ec1cd3fad6d75507a56b9b507
iksagor0 <iksagor0@gmail.com>
2022-11-26 10:25:12 +0600

    clean App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-// import AddEmoji from "./Inheritance/AddEmoji";
-
-// import AddEmoji from "./Composition/AddEmoji";
-// import Text from "./Composition/Text";
-
-// import AddEmoji from "./Composition_two/AddEmoji";
-// import Bracket from "./Composition_two/Bracket";
-// import Text from "./Composition_two/Text";
-
-// import Bhutan from "./RenderProps/Bhutan";
-// import Maldives from "./RenderProps/Maldives";
-// import Text from "./RenderProps/Text";
-
-// import Bangladesh from "./RenderPropsTwo/Bangladesh";
-// import Text from "./RenderPropsTwo/Text";
-
 import React from "react";
 import UserDetails from "./Components/UserDetails";
-import ClickCounter from "./HOC/ClickButton";
-import HoverCount from "./HOC/HoverCount";
 
 // STYLES
 const styles = {
@@ -29,30 +11,6 @@ const styles = {
 function App() {
   return (
     <div style={styles} className="App">
-      {/* === Inheritance === */}
-      {/* <AddEmoji /> */}
-
-      {/* === Composition === */}
-      {/* <AddEmoji>{(emoji) => <Text emoji={emoji} />}</AddEmoji> */}
-
-      {/* === Composition Two === */}
-      {/* <Text>
-        {(text) => (
-          <AddEmoji text={text}>
-            {(textWithEmoji) => <Bracket textWithEmoji={textWithEmoji} />}
-          </AddEmoji>
-        )}
-      </Text> */}
-
-      {/* === RENDER PROPS === */}
-      {/* <Text render={(text) => <Bhutan text={text} />} />
-      <Text render={(text) => <Maldives text={text} />} /> */}
-
-      {/* === RENDER PROPS 2 === */}
-      {/* <Bangladesh render={(country) => <Text country={country} />} /> */}
-
-      <ClickCounter />
-      <HoverCount />
       <UserDetails />
     </div>
   );
